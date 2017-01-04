5f80b99ee3f45e4bb87bd186844e815bf9d6e261
zhengyuqin <zhengyuqin@youmi.net>
2017-01-04 15:27:36 +0800

    first

---
+++ obj/local/armeabi/objs/demo/demo.o.d
@@ -0,0 +1,161 @@
+/Users/apple/adt/projects/Shelldemo/obj/local/armeabi/objs/demo/demo.o: \
+ /Users/apple/adt/projects/Shelldemo/jni/demo.c \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/string.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/unistd.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/select.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/signal.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/limits.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/signal.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/ucontext.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/user.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysconf.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/capability.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/pathconf.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/elf.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/exec_elf.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/exec.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/mman.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/mman.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/mman.h \
+ /Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/mman-common.h
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/unistd.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/select.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/signal.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/signal.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/user.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/linux/capability.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/pathconf.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/elf.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/exec_elf.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/machine/exec.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/sys/mman.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm/mman.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/mman.h:
+
+/Users/apple/Library/Android/android-ndk-r10e/platforms/android-19/arch-arm/usr/include/asm-generic/mman-common.h:
